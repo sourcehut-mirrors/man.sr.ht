0316776a9daea1051c947c322be835c293db4b72
Conrad Hoffmann <ch@bitfehler.net>
2024-11-08 11:35:36 +0100

    Reduce setup.py to a stub
    All configuration is now in pyproject.toml, all tooling set up to use
    it. The stub remains (for now) to execute the call to `make`, as such
    calls are not supported by pyproject.toml.

--- setup.py
+++ setup.py
@@ -13,34 +13,4 @@ subp = subprocess.run([make, "SRHT_PATH=" + srht_path])
 if subp.returncode != 0:
     sys.exit(subp.returncode)
 
-ver = os.environ.get("PKGVER") or subprocess.run(['git', 'describe', '--tags'],
-      stdout=subprocess.PIPE).stdout.decode().strip()
-
-setup(
-  name = 'mansrht',
-  packages = [
-      'mansrht',
-      'mansrht.alembic',
-      'mansrht.alembic.versions',
-      'mansrht.blueprints',
-      'mansrht.types',
-  ],
-  version = ver,
-  description = 'man.sr.ht website',
-  author = 'Drew DeVault',
-  author_email = 'sir@cmpwn.com',
-  url = 'https://git.sr.ht/~sircmpwn/man.sr.ht',
-  install_requires = ['srht'],
-  license = 'AGPL-3.0',
-  package_data={
-      'mansrht': [
-          'templates/*.html',
-          'static/icons/*',
-          'static/*'
-      ]
-  },
-  scripts = [
-      'mansrht-initdb',
-      'mansrht-migrate',
-  ]
-)
+setup()
