cae784e5b15a340fe473b19fe261fe223b8a59ee
Drew DeVault <drew@ddevault.org>
2025-08-28 11:15:16 +0200

    web: remove delegated OAuth components

--- mansrht/app.py
+++ mansrht/app.py
@@ -2,30 +2,14 @@ from mansrht.types import User
 from srht.config import cfg
 from srht.database import DbSession
 from srht.flask import SrhtFlask
-from srht.oauth import AbstractOAuthService
 from urllib.parse import urlparse
 
 db = DbSession(cfg("man.sr.ht", "connection-string"))
 db.init()
 
-client_id = cfg("man.sr.ht", "oauth-client-id")
-client_secret = cfg("man.sr.ht", "oauth-client-secret")
-gitsrht_cid = cfg("git.sr.ht", "oauth-client-id")
-
-class ManOAuthService(AbstractOAuthService):
-    def __init__(self):
-        super().__init__(
-                client_id, client_secret,
-                required_scopes=[
-                    "profile",
-                    f"{gitsrht_cid}/data",
-                    f"{gitsrht_cid}/info:write",
-                ],
-                user_class=User)
-
 class ManApp(SrhtFlask):
     def __init__(self):
-        super().__init__("man.sr.ht", __name__, oauth_service=ManOAuthService())
+        super().__init__("man.sr.ht", __name__, user_class=User)
 
         from mansrht.blueprints.html import html
         from mansrht.blueprints.create import create
