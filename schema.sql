sourcehut-mirrors/man.sr.ht

commit d91f39a669bf9fd78f995221291037ae55bfc9fc

schema: remove backing_repo table
We don't need to maintain a separate copy of the git.sr.ht state,
updated via webhooks, which just gets stale.

schema.sql
@@ -50,7 +50,10 @@ CREATE TABLE wiki (
 	name character varying(256) NOT NULL,
 	owner_id integer NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
 	visibility visibility NOT NULL,
-	repo_id integer NOT NULL REFERENCES backing_repo(id)
+	-- git.sr.ht repository ID:
+	repo_id integer NOT NULL,
+	-- git repository reference:
+	repo_ref text NOT NULL
 );
 
 CREATE TABLE root_wiki (

mansrht/access.py
@@ -30,7 +30,6 @@ def get_repo_access(wiki, owner, user=None):
     if not user:
         user = current_user
     valid = Validation(request)
-    repo = wiki.repo
     resp = exec_gql("git.sr.ht", """
     query RepoAccess($repo: String!, $username: String!) {
         user(username: $username) {
@@ -40,7 +39,7 @@ def get_repo_access(wiki, owner, user=None):
         }
     }
     """, valid=valid, user=user,
-        username=owner.username, repo=repo.name)
+        username=owner.username, repo=wiki.repo_name)
     if not valid.ok:
         return UserAccess.none
     try:

mansrht/blueprints/create.py
@@ -5,7 +5,7 @@ from srht.oauth import current_user, loginrequired
 from srht.validation import Validation
 from mansrht.repo import GitsrhtBackend
 from mansrht.types import Wiki, Visibility
-from mansrht.wikis import validate_name, create_repo, create_wiki
+from mansrht.wikis import validate_name
 from collections import namedtuple
 import os
 
@@ -21,9 +21,8 @@ def select_repo(backend, wiki_name, wiki_visibility, **kwargs):
     ]
 
     existing = [
-        wiki.repo.name
+        wiki.repo_name
         for wiki in Wiki.query.filter(Wiki.owner_id == current_user.id).all()
-        if wiki.repo
     ]
 
     # TODO: Add cancel button.
@@ -146,27 +145,21 @@ def select_ref_POST():
             return select_repo(backend, wiki_name, visibility.value, **valid.kwargs)
         backend_repo = backend.create_repo(repo_name, repo_visibility)
 
-    # Try to find the latest commit if we're using an existing repo + ref.
-    new_ref = request.path.endswith("new")
-    commit = None
-    if not new_repo and not new_ref:
-        commit = backend.get_latest_commit(repo_name, ref_name)
-        valid.expect(
-                commit is not None,
-                "Ref was not found.",
-                field="ref")
-    if not valid.ok:
-        return select_ref(backend, wiki_name, repo_name,
-                repo_visibility, new_repo, **valid.kwargs)
-
     backend.ensure_repo_update()
 
-    repo = create_repo(
-            new_repo, backend_repo["name"], backend_repo["id"], ref_name,
-            current_user, commit=commit)
-    create_wiki(
-            valid, current_user, wiki_name,
-            repo, visibility, is_root=is_root)
+    wiki = Wiki()
+    wiki.name = wiki_name
+    wiki.owner_id = current_user.id
+    wiki.visibility = visibility
+    wiki.repo_name = backend_repo["name"]
+    wiki.repo_ref = ref_name
+    db.session.add(wiki)
+
+    if is_root:
+        root_wiki = RootWiki(id=wiki.id)
+        db.session.add(root_wiki)
+
+    db.session.commit()
 
     del session["wiki_name"]
     del session["wiki_repo"]

mansrht/blueprints/html.py
@@ -10,7 +10,7 @@ from srht.cache import set_cache, get_cache
 from srht.crypto import encrypt_request_authorization
 from srht.validation import Validation
 from mansrht.access import UserAccess, check_access
-from mansrht.repo import GitsrhtBackend
+from mansrht.repo import GitsrhtBackend, MissingRepositoryError
 from mansrht.types import User, Wiki, RootWiki
 from mansrht.wikis import is_root_wiki
 from prometheus_client import Counter
@@ -42,29 +42,37 @@ def content(wiki, path, is_root=False, **kwargs):
 
     ssh_host = urlparse(backend.origin_ext).hostname
     clone_urls = {
-            "https": f"{backend.origin}/{wiki.owner}/{wiki.repo.name}",
-            "ssh": f"{backend.ssh_user}@{ssh_host}:{wiki.owner}/{wiki.repo.name}",
+            "https": f"{backend.origin}/{wiki.owner}/{wiki.repo_name}",
+            "ssh": f"{backend.ssh_user}@{ssh_host}:{wiki.owner}/{wiki.repo_name}",
     }
 
-    web_url = f"{backend.origin}/{wiki.owner.canonical_name}/{wiki.repo.name}"
-    if not wiki.repo.commit_sha:
-        return render_template("new-wiki.html", wiki=wiki,
-                clone_urls=clone_urls, repo=wiki.repo, web_url=web_url)
+    web_url = f"{backend.origin}/{wiki.owner.canonical_name}/{wiki.repo_name}"
 
     if not path:
         path = ""
 
     n = 0
-    item = backend.get_tree_entry(wiki.repo.name, wiki.repo.ref, path=path)
+    try:
+        item, commit = backend.get_tree_entry(wiki.repo_name, wiki.repo_ref, path=path)
+    except MissingRepositoryError:
+        if current_user == wiki.owner:
+            return render_template("missing-repo.html",
+                wiki=wiki, backend=backend)
+        abort(404)
+
+    if not commit:
+        return render_template("new-wiki.html",
+            wiki=wiki, clone_urls=clone_urls, web_url=web_url)
+
     if item and item["object"]["type"] == "TREE" and not request.path.endswith("/"):
         if not is_root or path != "":
             return redirect(request.path + "/")
 
     while item and item["object"]["type"] == "TREE" and n < 5:
-        item = backend.get_tree_entry(wiki.repo.name, wiki.repo.ref,
+        item, commit = backend.get_tree_entry(wiki.repo_name, wiki.repo_ref,
             path=os.path.join(path, "index.md"))
         if not item and is_root:
-            item = backend.get_tree_entry(wiki.repo.name, wiki.repo.ref,
+            item, commit = backend.get_tree_entry(wiki.repo_name, wiki.repo_ref,
                 path=os.path.join(path, "index.html"))
         n += 1
     if not item or item["object"]["type"] != "BLOB":
@@ -75,7 +83,7 @@ def content(wiki, path, is_root=False, **kwargs):
 
     blob_id = item["object"]["id"]
     blob_name = item["name"]
-    cachekey = f"{wiki.repo.name}:{blob_id}"
+    cachekey = f"{wiki.repo_name}:{blob_id}"
     html_cachekey = f"man.sr.ht:content:{cachekey}:v{SRHT_MARKDOWN_VERSION}:v6"
     frontmatter_cachekey = f"man.sr.ht:frontmatter:{cachekey}"
     html = get_cache(html_cachekey)
@@ -151,9 +159,9 @@ def content(wiki, path, is_root=False, **kwargs):
 
     return render_template("content.html",
             content=Markup(soup), firstpara=Markup(firstpara),
-            title=title, repo=wiki.repo, toc=toc, wiki=wiki, is_root=is_root,
+            title=title, toc=toc, wiki=wiki, is_root=is_root,
             path=path, frontmatter=frontmatter, clone_urls=clone_urls,
-            web_url=web_url, **kwargs)
+            web_url=web_url, commit=commit, **kwargs)
 
 @html.route("/")
 @html.route("/<path:path>")

mansrht/blueprints/manage.py
@@ -28,19 +28,16 @@ def info_POST(owner_name, wiki_name):
             cls=Visibility, default=wiki.visibility)
     wiki.visibility = visibility
     db.session.commit()
-    return redirect(url_for(
-        "manage.info", owner_name=owner_name, wiki_name=wiki_name))
+    return redirect(url_for("manage.info",
+        owner_name=owner_name, wiki_name=wiki_name))
 
 @manage.route("/manage/<owner_name>/<wiki_name>/delete")
 @loginrequired
 def delete(owner_name, wiki_name):
     # check_access() guarantees owner and wiki are valid.
     owner, wiki = check_access(owner_name, wiki_name, UserAccess.manage)
-    return render_template(
-            "delete.html", owner=owner, wiki=wiki,
-            # Fill default value for "delete repo" option based-on if the
-            # wiki was created with a new repo in the first place.
-            delete_repo=wiki.repo.new)
+    backend = GitsrhtBackend(owner)
+    return render_template("delete.html", owner=owner, wiki=wiki, backend=backend)
 
 @manage.route("/manage/<owner_name>/<wiki_name>/delete", methods=["POST"])
 @loginrequired
@@ -50,10 +47,5 @@ def delete_POST(owner_name, wiki_name):
 
     # check_access() guarantees owner and wiki are valid.
     owner, wiki = check_access(owner_name, wiki_name, UserAccess.manage)
-    backend = GitsrhtBackend(owner)
-    try:
-        backend.unensure_repo_postupdate(wiki.repo)
-    except:
-        pass # Deleted, presumably
     delete_wiki(wiki, owner, delete_repo == "on")
     return redirect("/")

mansrht/blueprints/notify.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 from srht.crypto import verify_request_signature
 from srht.database import db
 from srht.flask import csrf_bypass
-from mansrht.types import User, Wiki, BackingRepo
+from mansrht.types import User, Wiki
 from mansrht.wikis import delete_wiki
 import json
 
@@ -11,60 +11,9 @@ webhooks_notify = Blueprint("webhooks_notify", __name__)
 @csrf_bypass
 @webhooks_notify.route("/webhook/notify/<repo_id>/refs", methods=["POST"])
 def ref_update(repo_id):
-    event = request.headers.get("X-Webhook-Event")
-    payload = verify_request_signature(request)
-    payload = json.loads(payload.decode("utf-8"))
-    if event != "repo:post-update":
-        return f"Unexpected event {event}"
-    wiki = (Wiki.query.join(Wiki.repo)
-            .filter(BackingRepo.id == repo_id)).one_or_none()
-    if not wiki:
-        return "I don't recognize this wiki"
-    repo = wiki.repo
-    for ref in payload["refs"]:
-        if ref["name"] == f"refs/heads/{repo.ref}":
-            commit = ref.get("new")
-            if commit:
-                repo.commit_sha = commit["id"]
-                repo.commit_author = commit["author"]["name"]
-                repo.commit_email = commit["author"]["email"]
-                repo.commit_time = commit["timestamp"]
-                repo.commit_message = commit["message"]
-                repo.tree_sha = commit["tree"]
-            else:
-                # Nullify all the fields so that the wiki defaults back to the
-                # "new-wiki" page.
-                repo.commit_sha = None
-                repo.commit_author = None
-                repo.commit_email = None
-                repo.commit_time = None
-                repo.commit_message = None
-                repo.tree_sha = None
-            db.session.commit()
-            break
-    else:
-        return "No wikis updated"
-    return "Updated wiki SHA, thanks!"
+    pass # TODO: Remove me in a near-future commit
 
 @csrf_bypass
 @webhooks_notify.route("/webhook/notify/repos/update", methods=["POST"])
 def repo_update():
-    event = request.headers.get("X-Webhook-Event")
-    payload = json.loads(request.data.decode("utf-8"))
-    if event not in ["repo:update", "repo:delete"]:
-        return f"Unexpected event {event}"
-    repo = BackingRepo.query.filter(
-            BackingRepo.resource_id == payload["id"]).one_or_none()
-    if not repo:
-        return "Unknown repo"
-    if event == "repo:update":
-        if repo and payload["name"] != repo.name:
-            repo.name = payload["name"]
-            db.session.commit()
-            return "Updated repo name"
-    elif event == "repo:delete":
-        wiki = Wiki.query.filter(Wiki.repo_id == repo.id).one_or_none()
-        wiki_name = wiki.name
-        delete_wiki(wiki, wiki.owner, delete_from_backend=False)
-        return f"Deleted wiki {wiki_name}"
-    return "No repos updated"
+    pass # TODO: Remove me in a near-future commit

mansrht/repo.py
@@ -8,6 +8,9 @@ import os
 origin = get_origin("man.sr.ht")
 git_user = cfg("git.sr.ht", "ssh-user", "git")
 
+class MissingRepositoryError(Exception):
+    pass
+
 class GitsrhtBackend():
     """
     git.sr.ht-based backend for man.sr.ht.
@@ -112,44 +115,44 @@ class GitsrhtBackend():
         return os.path.join(self.origin_ext,
                 self.owner.canonical_name, repo_name, ref)
 
-    def get_latest_commit(self, repo_name, ref):
-        r = exec_gql("git.sr.ht", """
-        query GetLatestCommit($name: String!, $ref: String!) {
-            me {
-                repository(name: $name) {
-                    log(from: $ref) {
-                        results {
-                            id
-                            author {
-                                name
-                                email
-                                time
-                            }
-                            message
-                            tree {
-                                id
+    def get_tree_entry(self, repo_name, branch, path=None):
+        # Root path is a special case
+        if not path:
+            r = exec_gql("git.sr.ht", """
+            query GetLatestCommit($name: String!, $ref: String!) {
+                me {
+                    repository(name: $name) {
+                        reference(name: $ref) {
+                            follow {
+                                ... on Commit {
+                                    id
+                                    message
+                                    author {
+                                        name
+                                        email
+                                        time
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-        """, name=repo_name, ref=ref)
-        log = r["me"]["repository"]["log"]["results"]
-        if len(log) == 0:
-            return None
-        return log[0]
-
-    def get_tree_entry(self, repo_name, ref, path=None):
-        if not path:
-            return { "object": { "type": "TREE" } } # root
+            """, name=repo_name, ref=f"refs/heads/{branch}", user=self.owner)
+            commit = None
+            repo = r["me"]["repository"]
+            if not repo:
+                raise MissingRepositoryError()
+            if repo["reference"]:
+                commit = r["me"]["repository"]["reference"]["follow"]
+            return { "object": { "type": "TREE" } }, commit # root
 
         path = path.rstrip("/")
         r = exec_gql("git.sr.ht", """
-        query GetTree($name: String!, $ref: String!, $path: String!) {
+        query GetTree($name: String!, $ref: String!, $branch: String!, $path: String!) {
             me {
                 repository(name: $name) {
-                    path(revspec: $ref, path: $path) {
+                    path(revspec: $branch, path: $path) {
                         name
                         object {
                             id
@@ -163,11 +166,35 @@ class GitsrhtBackend():
                             }
                         }
                     }
+
+                    reference(name: $ref) {
+                        follow {
+                            ... on Commit {
+                                id
+                                message
+                                author {
+                                    name
+                                    email
+                                    time
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        """, name=repo_name, ref=ref, path=path, user=self.owner)
-        return r["me"]["repository"]["path"]
+        """,
+                 name=repo_name, branch=branch, ref=f"refs/heads/{branch}",
+                 path=path, user=self.owner)
+
+        repo = r["me"]["repository"]
+        if not repo:
+            raise MissingRepositoryError()
+        path = repo["path"]
+        commit = None
+        if repo["reference"]:
+            commit = repo["reference"]["follow"]
+        return path, commit
 
     # TODO: Drop webhooks
     def ensure_repo_postupdate(self, repo):

mansrht/templates/content.html
@@ -24,13 +24,13 @@
       <li class="nav-item">
         <a
           class="nav-link"
-          href="{{web_url}}/tree/{{repo.ref}}/{{'/'.join(path)}}?view-source"
+          href="{{web_url}}/tree/{{wiki.repo_ref}}/{{'/'.join(path)}}?view-source"
         >source</a>
       </li>
       <li class="nav-item">
         <a
           class="nav-link"
-          href="{{web_url}}/log/{{repo.ref}}"
+          href="{{web_url}}/log/{{wiki.repo_ref}}"
         >history</a>
       </li>
     </ul>
@@ -95,14 +95,14 @@
   <div class="row">
     <div class="col-md-8">
       <pre>commit <a
-href="{{web_url}}/commit/{{repo.commit_sha}}"
->{{ repo.commit_sha }}</a>
-Author: {{ repo.commit_author }} &lt;{{ repo.commit_email }}&gt;
-Date:   {{ repo.commit_time }}
+href="{{web_url}}/commit/{{commit.id}}"
+>{{ commit.id }}</a>
+Author: {{ commit.author.name }} &lt;{{ commit.author.email }}&gt;
+Date:   {{ commit.author.time }}
 
-{{ repo.commit_message.rstrip("\n") }}</pre>
+{{ commit.message.rstrip("\n") }}</pre>
       <dl>
-        <dt><strong>Clone this wiki</strong>{{" (\"" ~ repo.ref ~ "\" branch)"}}</dt>
+        <dt><strong>Clone this wiki</strong>{{" (\"" ~ wiki.repo_ref ~ "\" branch)"}}</dt>
         <dd>
           <a href={{ clone_urls.https }}>{{ clone_urls.https }}</a>
           <span class="text-muted pull-right">(read-only)</span><br />

commit acd6b461b3878be777bfc02f7fd40d9a180341ad

schema.sql: resync with latest schema for wiki table
Due to a "typo", commit d91f39a6 introduces a desynchronization between
schema.sql and the actual data model for that table, making man.sr.ht
unusable if built from the ground up.

Fixes: d91f39a6 ("schema: remove backing_repo table")
Signed-off-by: Simon Martin <simon@nasilyan.com>

schema.sql
@@ -50,8 +50,8 @@ CREATE TABLE wiki (
 	name character varying(256) NOT NULL,
 	owner_id integer NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
 	visibility visibility NOT NULL,
-	-- git.sr.ht repository ID:
-	repo_id integer NOT NULL,
+	-- git.sr.ht repository name:
+	repo_name text NOT NULL,
 	-- git repository reference:
 	repo_ref text NOT NULL
 );